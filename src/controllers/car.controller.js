ff9680214b5a76b65ffd75c4e4f0d73b27b57ddf
Juan <juandiegovelsol@gmail.com>
2023-05-21 15:06:41 -0500

    Feat: admin is able to update car quantity, all users are able to know one car data

--- src/controllers/car.controller.js
+++ src/controllers/car.controller.js
@@ -21,3 +21,36 @@ export const getAllCars = async (req, res) => {
     res.status(500).json({ error: error });
   }
 };
+
+export const updateQuantity = async (req, res) => {
+  const { id2 } = req.params;
+  try {
+    const { quantity } = req.body;
+    const car = await prisma.car.update({
+      where: {
+        idcar: +id2,
+      },
+      data: {
+        quantity: +quantity,
+      },
+    });
+    res.status(200).json(car);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const getOneCar = async (req, res) => {
+  try {
+    const { model, color } = req.body;
+    const car = await prisma.car.findFirst({
+      where: {
+        model: model,
+        color: color,
+      },
+    });
+    res.status(200).json(car);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { createCar, getAllCars } from "../controllers/car.controller.js";
+import {
+  createCar,
+  getAllCars,
+  updateQuantity,
+  getOneCar,
+} from "../controllers/car.controller.js";
 import { isAuthenticaded } from "../middlewares/isAuthenticaded.middle.js";
 import { isAdmin } from "../middlewares/isAdmin.middle.js";
 
@@ -7,5 +12,7 @@ const router = express.Router();
 
 router.post("/:id", isAuthenticaded, isAdmin, createCar);
 router.get("/:id", isAuthenticaded, isAdmin, getAllCars);
+router.put("/:id/:id2", isAuthenticaded, isAdmin, updateQuantity);
+router.get("/info/:id", isAuthenticaded, getOneCar);
 
 export default router;
