ceefb53fddd42568a41f193260f4645ca68c7978
Juan <juandiegovelsol@gmail.com>
2023-05-21 17:42:00 -0500

    Feat: user is able to cancel order only if order state is pending

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -4,6 +4,7 @@ import {
   getAllOrders,
   getUserOrders,
   adminUpdateOrder,
+  userCancelOrder,
 } from "../controllers/order.controller.js";
 import { isAuthenticaded } from "../middlewares/isAuthenticaded.middle.js";
 import { isAdmin } from "../middlewares/isAdmin.middle.js";
@@ -14,5 +15,6 @@ router.post("/", isAuthenticaded, createOrder);
 router.get("/auth/:id", isAuthenticaded, isAdmin, getAllOrders);
 router.get("/:id", isAuthenticaded, getUserOrders);
 router.put("/:id/:id2", isAuthenticaded, isAdmin, adminUpdateOrder);
+router.put("/:id", isAuthenticaded, userCancelOrder);
 
 export default router;
